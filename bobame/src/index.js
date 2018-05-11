9b9f06c0d3d5fa15f420a18a14a8e1f298386029
Vince Chang <vincechang@Vinces-MacBook-Pro.local>
2018-05-10 22:49:48 -0700

    Added route for home page and set up data for boba shops. Weeded out things I didn't need by default from create-react-app

--- bobame/src/index.js
+++ bobame/src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
+import { HashRouter } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// Changing up the color and font scheme for the app
+// const bobaTheme = getMuiTheme({});
+
+ReactDOM.render((
+  <MuiThemeProvider bobaTheme={bobaTheme}>
+    <div>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </div>
+  </MuiThemeProvider>)
+  ,document.getElementById('root'));
